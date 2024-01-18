099ced814610a292975e192b91da2e504e17ffa4
bardoor <dmitrybartenev58@gmail.com>
2024-01-19 01:50:15 +0300

    Добавил функцию execute()
    Она распознаёт команду и исполняет её

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+variables = {}
+
+
 def get_instruction(code_line: str) -> dict:
     instruction = {}
     if ":=" in code_line:
@@ -7,3 +10,11 @@ def get_instruction(code_line: str) -> dict:
         instruction = {"operation": "assign", "variable_name": variable_name, "value": value}
 
     return instruction
+
+
+def execute(code_line: str) -> None:
+    instruction = get_instruction(code_line)
+    if instruction["operation"] == "assign":
+        variable_name = instruction["variable_name"]
+        value = instruction["value"]
+        variables[vatiable_name] = value
