27cac8b8f7a82839679f227587b338e1231dfe4d
bardoor <dmitrybartenev58@gmail.com>
2024-01-19 02:17:11 +0300

    Добавил бесконечный ввод команд

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ variables = {}
 
 def get_instruction(code_line: str) -> dict:
     instruction = {}
+
+    # Если в строке кода содержится оператор присваивания
     if ":=" in code_line:
-        components = code_line.split(" ")
+        components = code_line.split(" ") # Разделить строку кода по пробелам
         variable_name = components[0]
         value = components[2]
         instruction = {"operation": "assign", "variable_name": variable_name, "value": value}
@@ -17,4 +19,12 @@ def execute(code_line: str) -> None:
     if instruction["operation"] == "assign":
         variable_name = instruction["variable_name"]
         value = instruction["value"]
-        variables[vatiable_name] = value
+        variables[variable_name] = value
+
+
+if __name__ == '__main__':
+    print("Вас приветствует консоль! Удивительно, не правда ли? Вводите команды.")
+    while True:
+        command = input(">>> ")
+        execute(command)
+        print("Переменные ")
