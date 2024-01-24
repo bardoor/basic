26b103dc2d27a85adb591fbcdedb3a6e1a7a34b6
shadowflexer <ikhanin2008@mail.ru>
2024-01-24 17:06:11 +0300

    условия div
    Создал условие, проверяющее строку на команду разделить

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def get_instruction(code_line: str) -> dict:
         variable_name = components[0]
         value = components[2]
         instruction = {"operation": "assign", "variable_name": variable_name, "value": value}
+    elif code_line[0:3] == "div":
+        components = code_line.split(" ")
+        variable_name = components[1]
+        podelit_na = components[2]
+        instruction = {"operation": "div", "variable_name": variable_name, "podelit_na": podelit_na}
 
     return instruction
 
